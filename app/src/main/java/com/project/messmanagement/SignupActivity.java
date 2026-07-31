c0b5df7385db8b977552b394c79d98a4d4b61a5a
unknown <2023-3-60-051@std.ewubd.edu>
2026-08-01 00:53:35 +0600

    Fixed the Sign Up error in android

--- app/src/main/java/com/project/messmanagement/SignupActivity.java
+++ app/src/main/java/com/project/messmanagement/SignupActivity.java
@@ -2,16 +2,16 @@ package com.project.messmanagement;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText etFullName, etEmail, etPhone, etPassword;
-    private LinearLayout btnContinue;
+    private TextInputEditText etFullName, etEmail, etPhone, etPassword;
+    private MaterialButton btnContinue;
     private TextView tvSignInLink;
     private AppDatabase database;
 
